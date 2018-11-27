b850d41bb5835754aa3026f304f75b8c99fadb9e
Jenna Mills <superangelfish555@gmail.com>
2018-11-27 08:15:42 -0700

    Finished building Frame class.

--- src/november/view/NovemberFrame.java
+++ src/november/view/NovemberFrame.java
@@ -1,6 +1,31 @@
 package november.view;
 
-public class NovemberFrame
+import javax.swing.JFrame;
+import november.controller.NovemberController;
+
+public class NovemberFrame extends JFrame
 {
+	private NovemberController appController;
+	private NovemberPanel appPanel;
+	
+	public NovemberFrame(NovemberController appController)
+	{
+		super();
+		
+		this.appController = appController;
+		this.appPanel = new NovemberPanel(appController);
+		
+		setupFrame();
+		
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setSize(1000, 500);;
+		this.setTitle("November GUI App");
+		this.setResizable(false);;
+		this.setVisible(true);
+	}
 
 }
