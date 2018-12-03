b4c05ca19c49e3fe4eb86a7dede9449b0e33877a
Jenna Mills <superangelfish555@gmail.com>
2018-12-03 09:05:14 -0700

    Installed window builder, added a new button called funButton, I tried to make it work but so far it does not. Moved all the GUI constraint code to the setupLayout dump. Imported springLayout.

--- src/november/view/NovemberPanel.java
+++ src/november/view/NovemberPanel.java
@@ -11,16 +11,21 @@ public class NovemberPanel extends JPanel
 	private NovemberController appController;
 	private JLabel textLabel;
 	private JButton colorButton;
+	private SpringLayout appLayout;
+	private JButton funButton;
 	
 	public NovemberPanel(NovemberController appController)
 	{
 		super();
 		
 		this.appController = appController;
-		
-		colorButton = new JButton("Click to change the color");
 		textLabel = new JLabel("This is a color app");
+		colorButton = new JButton("tap me to change background color");
+		appLayout = new SpringLayout();
+		funButton = new JButton("tap me to change button color");
+
 		
+
 		setupPanel();
 		setupLayout();
 		setupListeners();
@@ -28,16 +33,24 @@ public class NovemberPanel extends JPanel
 	
 	private void setupPanel()
 	{
-		this.setBackground(Color.blue);
+		this.setLayout(appLayout);
+		this.setBackground(Color.orange);
 		this.add(textLabel);
 		this.add(colorButton);
+		this.add(funButton);
 		
 		
 	}
 
-	private void setupLayout()
+	private void setupLayout() //stupid GUI code dump
 	{
+		appLayout.putConstraint(SpringLayout.NORTH, funButton, 6, SpringLayout.SOUTH, colorButton);
+		appLayout.putConstraint(SpringLayout.EAST, funButton, -10, SpringLayout.EAST, this);
+		appLayout.putConstraint(SpringLayout.EAST, colorButton, 0, SpringLayout.EAST, funButton);
 		
+		appLayout.putConstraint(SpringLayout.NORTH, colorButton, 40, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.NORTH, textLabel, 10, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.WEST, textLabel, 10, SpringLayout.WEST, this);
 	}
 	
 	private void setupListeners()
@@ -50,16 +63,34 @@ public class NovemberPanel extends JPanel
 			}
 		
 		});
+		
+		funButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent mouseClick)
+			{
+				changeFunButtonColor();
+			}
+		});
 	}
 	
 	private void changeBackgroundColor()
 	{
-		int red = (int)(Math.random() * 256);
-		int green = (int)(Math.random() * 256);
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
 		int blue = (int) (Math.random() * 256);
 		
 		this.setBackground(new Color(red, green, blue));
-		textLabel.setText("red is " + red + " Green is: " + green + "Blue is: " + blue);
+		textLabel.setText("Red is " + red + "\nGreen is: " + green + "\nBlue is: " + blue);
+	}
+	private void changeFunButtonColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		
+		funButton.setBackground(new Color(red, green, blue));
+		colorButton.setBackground(new Color(red, green, blue));
+		textLabel.setBackground(new Color(red, green, blue));
 	}
 }
 
